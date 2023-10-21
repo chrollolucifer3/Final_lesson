7d2b4810f491229496c0d16fbf97fd9031cfef03
Nguyen_Xuan_Cuong <chrollolucifer97@gmail.com>
2023-10-21 22:20:46 +0700

    second commit

--- js/main.js
+++ js/main.js
@@ -26,5 +26,20 @@ $('.carousel-5').owlCarousel({
     }
 })
 
-//Phần card 
+//Phần back-top
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop()) {
+            $('#back-top').fadeIn();
+        } else {
+            $('#back-top').fadeOut();
+        }
+    })
+
+    $('#back-top').click(function() {
+        $('html, body').animate( {
+            scrollTop: 0
+        }, 300)
+    })
+})
 
