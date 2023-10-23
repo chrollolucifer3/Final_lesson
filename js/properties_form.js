0591e5e640708694258e276a65d143cb2d61bc6c
Nguyen_Xuan_Cuong <chrollolucifer97@gmail.com>
2023-10-23 17:16:07 +0700

    second commit

--- js/properties_form.js
+++ js/properties_form.js
@@ -1,14 +1,19 @@
-// JavaScript
 document.addEventListener("DOMContentLoaded", function () {
     const advancedToggle = document.getElementById("advanced-toggle");
     const checkboxes = document.querySelector(".checkbox");
+    const advancedIcon = advancedToggle.querySelector("i");
 
-    advancedToggle.addEventListener("click", function () {
-        checkboxes.classList.toggle("hidden");
+    advancedToggle.addEventListener("click", function (e) {
+        e.preventDefault();
         checkboxes.classList.toggle("visible");
-        
-        const advancedIcon = document.getElementById("advanced-icon");
-        advancedIcon.classList.toggle("fa-plus");
-        advancedIcon.classList.toggle("fa-minus");
+        checkboxes.classList.toggle("hidden");
+
+        if (advancedIcon.classList.contains("fa-plus")) {
+            advancedIcon.classList.remove("fa-plus");
+            advancedIcon.classList.add("fa-minus");
+        } else {
+            advancedIcon.classList.remove("fa-minus");
+            advancedIcon.classList.add("fa-plus");
+        }
     });
 });
