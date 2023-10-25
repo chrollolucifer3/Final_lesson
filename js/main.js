fc1fb8a4425244af488955df2d8425cc1666c139
Nguyen_Xuan_Cuong <chrollolucifer97@gmail.com>
2023-10-26 02:37:14 +0700

    last commit

--- js/main.js
+++ js/main.js
@@ -17,11 +17,33 @@ $('.carousel-5').owlCarousel({
         0:{
             items:1
         },
-        600:{
+        412:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        1024:{
+            items:4
+        }
+    }
+})
+
+$('.carousel-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:6000,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
             items:2
         },
         1000:{
-            items:4
+            items:2
         }
     }
 })
